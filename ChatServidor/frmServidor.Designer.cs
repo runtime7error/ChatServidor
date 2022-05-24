274014763acaadc539e7dd659497d9a631e1941e
Lucas Henrique <validacao01.inobag@gmail.com>
2022-05-24 06:59:11 -0300

    -Adição de uma foto de background;

--- ChatServidor/frmServidor.Designer.cs
+++ ChatServidor/frmServidor.Designer.cs
@@ -36,34 +36,38 @@
             // 
             // txtLog
             // 
-            this.txtLog.Location = new System.Drawing.Point(13, 64);
+            this.txtLog.Location = new System.Drawing.Point(17, 79);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(390, 229);
+            this.txtLog.Size = new System.Drawing.Size(519, 281);
             this.txtLog.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Location = new System.Drawing.Point(17, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(97, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Endereço IP : ";
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(94, 30);
+            this.txtIP.Location = new System.Drawing.Point(125, 37);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(179, 20);
+            this.txtIP.Size = new System.Drawing.Size(237, 22);
             this.txtIP.TabIndex = 2;
             // 
             // btnAtender
             // 
-            this.btnAtender.Location = new System.Drawing.Point(279, 27);
+            this.btnAtender.Location = new System.Drawing.Point(372, 33);
+            this.btnAtender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAtender.Name = "btnAtender";
-            this.btnAtender.Size = new System.Drawing.Size(124, 23);
+            this.btnAtender.Size = new System.Drawing.Size(165, 28);
             this.btnAtender.TabIndex = 3;
             this.btnAtender.Text = "Iniciar Atendimento";
             this.btnAtender.UseVisualStyleBackColor = true;
@@ -71,14 +75,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(418, 305);
+            this.BackgroundImage = global::ChatServidor.Properties.Resources.PCdjDu;
+            this.ClientSize = new System.Drawing.Size(557, 375);
             this.Controls.Add(this.btnAtender);
             this.Controls.Add(this.txtIP);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLog);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Chat Servidor";
             this.ResumeLayout(false);
